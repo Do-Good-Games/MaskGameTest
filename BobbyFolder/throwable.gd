23e5afb1d8fbccd8c234f5867d7e5b88750be142
Robert Breglio <43708172+pwnzor79@users.noreply.github.com>
2026-01-30 20:44:16 -0700

    thrown objects stop in time

--- BobbyFolder/throwable.gd
+++ BobbyFolder/throwable.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
-@onready var speed = 40
+@onready var speed = 60
+var throwtime = 120
 
 func _ready() -> void:
 	print("i live")
 	move_toward(10, 20, 50)
 
 func _physics_process(delta):
-	#velocity = velocity.lerp(Vector2.from_angle(0) * 20, 1)
-	velocity = transform.x * speed
+	
+	if(throwtime >= 0):
+		throwtime = throwtime - 1
+		velocity = transform.x * speed
+	else:
+		velocity = transform.x * 0
+	
 	move_and_slide()
