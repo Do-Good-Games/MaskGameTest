2b0a1b308af6d532b9fac64a6cbf9fe9336047ca
Cyberebral <spiftastic@msn.com>
2026-02-19 12:11:05 -0700

    patched holes in level geometry
    - updated level mask maker

--- Art/NeeaFolder/Scripts/beh_save_masks.gd
+++ Art/NeeaFolder/Scripts/beh_save_masks.gd
@@ -10,6 +10,7 @@ const LAYERS_ALL : int = 1048575
 const WINDOW_SIZE = Vector2i(1920, 1080)
 
 @export var disable_render : bool
+@export var name_prefix : String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,7 +24,30 @@ func _ready() -> void:
 	var viewport : Viewport = get_viewport()
 	#print(viewport.name)
 	
-	var render_scene : Node3D = viewport.get_children()[-1]
+	# get all level scenes based on name pattern and type
+	var scenes : Array[Node] = viewport.find_children("lvl*_*_?D", "Node3D", false)
+	
+	# get original scene visibility
+	var scene_visible : Array[bool]
+	for i : int in range(len(scenes)):
+		scene_visible.append(scenes[i].is_visible())
+		#print(scenes[i].name, scene_visible[-1])
+	
+	# hide all scenes
+	for scene : Node3D in scenes:
+		scene.set_visible(false)
+	
+	# reveal, save masks, hide
+	for i : int in range(len(scenes)):
+		if not scene_visible[i]: continue
+		
+		scenes[i].set_visible(true)
+		save_masks(viewport, scenes[i])
+		#scenes[i].set_visible(false)
+		break
+
+func save_masks(viewport : Viewport, render_scene : Node3D) -> void:
+	#var render_scene : Node3D = viewport.get_children()[-1]
 	var scene_name : String = render_scene.name
 	
 	# build file path base
@@ -35,23 +59,17 @@ func _ready() -> void:
 		print("scene root node name is incorrect:\nFormat: lvl<number>_<color>_3D\nCurrent Name:" + scene_name)
 		return
 	
-	# exit if name does not have level defined
-	if name_parts[LEVEL].find("lvl") < 0:
-		print("scene root node name is incorrect:\nFormat: lvl<number>_<color>_3D\nCurrent Name:" + scene_name)
-		return
-	
-	# exit if name does not have dimension defined
-	if name_parts[DIMENSION].find("D") < 0:
-		print("scene root node name is incorrect:\nFormat: lvl<number>_<color>_3D\nCurrent Name:" + scene_name)
-		return
+	var file_name_prefix : String
+	if len(name_prefix) > 0: file_name_prefix = name_prefix + delim
 	
 	var folder_name : String = "Level" + name_parts[LEVEL][-1] + name_parts[DIMENSION]
-	var file_name_base : String = name_parts[LEVEL] + delim + name_parts[COLOR] + delim
+	var file_name_base : String = file_name_prefix + name_parts[LEVEL] + delim + name_parts[COLOR] + delim
 	var file_path_base : String = "res://Levels/" + folder_name + "/"
 	
 	# get camera
 	var cam : Camera3D = viewport.get_camera_3d()
 	
+	# save masks
 	for layer : String in LAYERS_MASK.keys():
 		# change camera mask
 		cam.cull_mask = 0;
