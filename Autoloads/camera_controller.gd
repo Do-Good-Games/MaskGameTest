f4d1b2738012d6688224807b91ad1a471286a5ff
Cyberebral <spiftastic@msn.com>
2026-02-20 13:49:39 -0700

    Revert "make masks stay put!"
    This reverts commit 6641f1049b0820c6e7150e7b33a29e33215f2dff.

--- Autoloads/camera_controller.gd
+++ Autoloads/camera_controller.gd
@@ -3,18 +3,12 @@ extends Node
 @export var camera_depth: float  = 26
 
 
-#var scale_mtp = .01
-var scale_mtp = 40.0 / 1510.0 # convert 2D pixel to 3D world coordinates
-# this is based on the mask image width in pixels and the orthogonal camera used to generate them in the scn_create_masks scene
-# width is used because the orthogonal camera keeps the width when changing aspect ratios to match the level size
-#var offset_mtp = Vector2((1510.0 * scale_mtp) / -2.0, (1080.0 * scale_mtp) / -2.0)
-
+var scale_mtp = .01
 var Cameras : Dictionary[GameManager.color_enum, Camera3D]
 
 func set_camera_pos_init(new_pos_v2: Vector2):
 	
 	var new_pos_v3 = (Vector3(new_pos_v2.x * scale_mtp, camera_depth , new_pos_v2.y * scale_mtp))
-	#var new_pos_v3 = (Vector3(new_pos_v2.x * scale_mtp + offset_mtp.x, camera_depth , new_pos_v2.y * scale_mtp + offset_mtp.y))
 	
 	for key in Cameras.keys():
 		print("pos ", str(Cameras[key].global_position))
@@ -37,7 +31,6 @@ func usePlayer(player:BobbyCharacter):
 func update_cameras_v2(new_pos_v2: Vector2):
 	#new_pos_v2.
 	update_cameras_v3(Vector3(new_pos_v2.x * scale_mtp, camera_depth , new_pos_v2.y * scale_mtp))
-	#update_cameras_v3(Vector3(new_pos_v2.x * scale_mtp + offset_mtp.x, camera_depth , new_pos_v2.y * scale_mtp + offset_mtp.y))
 	#update_cameras_v3(Vector3(new_pos_v2.x, new_pos_v2.y, camera_depth))
 	pass
 
