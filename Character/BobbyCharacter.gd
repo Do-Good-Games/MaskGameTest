14e2641cc954060938c8ca34880f8cf096c0d24b
RyanDavidsonDev <ryan.r.davidson.dev@gmail.com>
2026-03-01 12:45:30 -0700

    marginally better tooling for camera bounds

--- Character/BobbyCharacter.gd
+++ Character/BobbyCharacter.gd
@@ -1,3 +1,4 @@
+@tool
 class_name BobbyCharacter extends CharacterBody2D
 
 #const Throwable: PackedScene = preload("res://BobbyFolder/throwable.tscn")
@@ -15,17 +16,62 @@ var curr_held_lamp
 
 var throwSpeed = 0
 
+@export_category("Camera Bounds")
+@export var upper_left_cam_bound : Vector2 = Vector2(  -486, -864)
+@export var lower_right_cam_bound :Vector2 = Vector2(486 ,864 )
+
 @onready var _animated_sprite = $Sprite2D
 @onready var camera_2d: Camera2D = $Camera2D
 
 func ready():
+	if Engine.is_editor_hint() :
+		return
 	CameraController.set_camera_pos_init(global_position)
 	game_manager.player = self
+	
+	camera_2d.limit_top = upper_left_cam_bound.x
+	camera_2d.limit_left = upper_left_cam_bound.y
+	camera_2d.limit_bottom = lower_right_cam_bound.x
+	camera_2d.limit_right = lower_right_cam_bound.y
+
+func _draw() -> void:
+	if Engine.is_editor_hint():
+		#var gizmo :rect =
+		var rect : RectangleShape2D
+		
+		
+		#1     2
+		
+		#3     4
+		
+		
+		#draw_line(upper_left_cam_bound + global_position, Vector2(upper_left_cam_bound.x, lower_right_cam_bound.y) + global_position, Color.YELLOW)
+		#draw_line(upper_left_cam_bound + global_position, Vector2(upper_left_cam_bound.y, lower_right_cam_bound.x) + global_position, Color.YELLOW)
+		#
+		#draw_line(lower_right_cam_bound + global_position, Vector2(upper_left_cam_bound.x, lower_right_cam_bound.y) + global_position, Color.YELLOW)
+		#draw_line(lower_right_cam_bound + global_position, Vector2(upper_left_cam_bound.y, lower_right_cam_bound.x) + global_position, Color.YELLOW)
+		
+		
+		draw_line(upper_left_cam_bound,Vector2(upper_left_cam_bound.x, lower_right_cam_bound.y), Color.YELLOW)
+		draw_line(upper_left_cam_bound, Vector2(lower_right_cam_bound.x, upper_left_cam_bound.y), Color.YELLOW)
+		
+		draw_line(lower_right_cam_bound,Vector2(upper_left_cam_bound.x, lower_right_cam_bound.y), Color.YELLOW)
+		draw_line(upper_left_cam_bound, Vector2(lower_right_cam_bound.x, upper_left_cam_bound.y), Color.YELLOW)
+		
+		return
+		
 
 func _process(_delta):
-		_animated_sprite.play("run")
+	if Engine.is_editor_hint() :
+		
+		queue_redraw()
+		return
+	_animated_sprite.play("run")
 
 func get_move_input():
+	if Engine.is_editor_hint() :
+		return
+	
 	var input = Vector2()
 	if Input.is_action_pressed('right'):
 		input.x += 1
@@ -38,6 +84,9 @@ func get_move_input():
 	return input
 	
 func process_throwing():
+	if Engine.is_editor_hint() :
+		return
+	
 	var in_hand_obj = game_manager.current_held._obj_ref
 	var in_hand_obj_throwable :Throwable = get_throwable_child(in_hand_obj)
 	if(in_hand_obj_throwable != null):
@@ -65,12 +114,18 @@ func process_throwing():
 	return
 	
 func get_throwable_child(parent) -> Throwable:
+	if Engine.is_editor_hint() :
+		return
+	
 	for n in parent.get_children():
 		if n is Throwable:
 			return n
 	return null
 
 func _physics_process(delta):
+	if Engine.is_editor_hint() :
+		return
+	
 	# Looking Code
 	look_at(get_global_mouse_position())
 	# Movement Code
@@ -110,4 +165,7 @@ func _physics_process(delta):
 
 		
 func _on_hitbox_area_entered(area: Area2D) -> void:
+	if Engine.is_editor_hint() :
+		return
+	
 	pass # Replace with function body.
